0537adae45283427a4bae56ae7b588e4ad708cfc
Tatiana Iavoruk <61006466+Tatiana655@users.noreply.github.com>
2023-04-07 15:51:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,6 +12,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     t = 47
     alpha = 1 - p
+    n = len(x)
     sum_x_2 = sum([e ** 2 for e in x])
     q_l = chi2(2 * n).ppf(q=alpha / 2)
     q_r = chi2(2 * n).ppf(q=1 - alpha / 2)
